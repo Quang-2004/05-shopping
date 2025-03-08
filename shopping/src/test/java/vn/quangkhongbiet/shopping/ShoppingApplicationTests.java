609199ce10abec6c7fe3f3b43f60ff2d0fdc0a9b
Quang-2004 <nguyen.v.quangtp@gmail.com>
2025-03-08 16:27:11 +0700

    start project

---
+++ shopping/src/test/java/vn/quangkhongbiet/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.quangkhongbiet.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
