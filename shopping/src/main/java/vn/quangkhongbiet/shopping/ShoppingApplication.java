5b5c11f28e9aff16faf79946e0c6bd973ad55ff5
Quang-2004 <nguyen.v.quangtp@gmail.com>
2025-04-05 21:54:37 +0700

    ignore .env file

--- shopping/src/main/java/vn/quangkhongbiet/shopping/ShoppingApplication.java
+++ shopping/src/main/java/vn/quangkhongbiet/shopping/ShoppingApplication.java
@@ -3,12 +3,19 @@ package vn.quangkhongbiet.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 // @SpringBootApplication(exclude =
 // org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
+		// Load file .env
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(ShoppingApplication.class, args);
 	}
 
